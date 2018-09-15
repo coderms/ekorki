22e82c535c3514a89e3b619d8aabb7aeb981e856
coderms <maciejsowinski@o2.pl>
2018-09-15 10:33:28 +0200

    Making tests working

--- test/models/ogloszenie_test.rb
+++ test/models/ogloszenie_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class OgloszenieTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Zapis ogłoszenia do bazy danych" do
+    o = Ogloszenie.new
+    o.tytul = 'max work'
+    o.opis = 'opis ogloszenia'
+    o.telefon = '123 456 789'
+    o.imie = 'Max'
+    o.nazwisko = 'Kolonko'
+    o.email = 'max.kolonko@swing.pl'
+    o.kategoria = 'cat_1_2'
+    assert o.save
+  end
 end
