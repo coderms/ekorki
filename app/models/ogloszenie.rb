0dd80ee61d070171d9244eb32ca7dd0e8ef4d90e
coderms <maciejsowinski@o2.pl>
2019-03-16 22:56:29 +0100

    Updated edit add

--- app/models/ogloszenie.rb
+++ app/models/ogloszenie.rb
@@ -2,4 +2,35 @@ class Ogloszenie < ActiveRecord::Base
   self.table_name = "ogloszenia"
   # attr_accessor :tytul, :opis, :telefon
   
+  def plany_zajec
+    PlanZajec.where("ogloszenie_id = ?", self.id)
+  end
+  
+  def pon
+    plany_zajec.select {|plan| plan[:dzien_tyg] == 'Poniedziałek' }.any?
+  end
+  
+  def wt
+    plany_zajec.select {|plan| plan[:dzien_tyg] == 'Wtorek' }.any?
+  end
+  
+  def sr
+    plany_zajec.select {|plan| plan[:dzien_tyg] == 'Środa' }.any?
+  end
+  
+  def czw
+    plany_zajec.select {|plan| plan[:dzien_tyg] == 'Czwartek' }.any?
+  end
+  
+  def pt
+    plany_zajec.select {|plan| plan[:dzien_tyg] == 'Piątek' }.any?
+  end
+  
+  def sob
+    plany_zajec.select {|plan| plan[:dzien_tyg] == 'Sobota' }.any?
+  end
+  
+  def nie
+    plany_zajec.select {|plan| plan[:dzien_tyg] == 'Niedziela' }.any?
+  end
 end
